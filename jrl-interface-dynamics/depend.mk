76b8b4f0fa31b66a5c35cc59fbfbbdfc92520b26
Anthony Mallet <anthony.mallet@laas.fr>
2010-01-14 12:14:15 +0100

    [wip/jrl-interface-dynamics] Testing 1.15.0

---
+++ jrl-interface-dynamics/depend.mk
@@ -0,0 +1,39 @@
+# $LAAS: depend.mk 2010/01/14 12:12:08 mallet $
+#
+# Copyright (c) 2008-2010 LAAS/CNRS
+# All rights reserved.
+#
+# Redistribution  and  use in source   and binary forms,  with or without
+# modification, are permitted provided that  the following conditions are
+# met:
+#
+#   1. Redistributions  of  source code must  retain  the above copyright
+#      notice and this list of conditions.
+#   2. Redistributions in binary form must  reproduce the above copyright
+#      notice  and this list of  conditions in the documentation   and/or
+#      other materials provided with the distribution.
+#
+#                                       Anthony Mallet on Fri Apr  4 2008
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+JRL_INTERFACE_DYNAMICS_DEPEND_MK:=${JRL_INTERFACE_DYNAMICS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=				jrl-interface-dynamics
+endif
+
+ifeq (+,$(JRL_INTERFACE_DYNAMICS_DEPEND_MK))
+PREFER.jrl-interface-dynamics?=		robotpkg
+
+DEPEND_USE+=				jrl-interface-dynamics
+
+DEPEND_ABI.jrl-interface-dynamics?=	jrl-interface-dynamics>=1.15
+DEPEND_DIR.jrl-interface-dynamics?=	../../wip/jrl-interface-dynamics
+
+SYSTEM_SEARCH.jrl-interface-dynamics=\
+	include/robotDynamics/jrlDynamicRobot.h		\
+	lib/pkgconfig/abstractRobotDynamics.pc
+endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
