22e1600adb5f5c7ffbe6b20813bd85a702ad82d4
florent <florent@laas.fr>
2010-03-01 09:32:11 +0100

    [wip] Add hpp-gik, hpp-hik, hpp-localstepper in robotpkg-wip

---
+++ hpp-gik/depend.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (c) 2008-2009 LAAS/CNRS
+# All rights reserved.
+#
+# Redistribution and use  in source  and binary  forms,  with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#   1. Redistributions of  source  code must retain the  above copyright
+#      notice and this list of conditions.
+#   2. Redistributions in binary form must reproduce the above copyright
+#      notice and  this list of  conditions in the  documentation and/or
+#      other materials provided with the distribution.
+#
+#                                      Anthony Mallet on Wed May 14 2008
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+HPP_GIK_DEPEND_MK:=	${HPP_GIK_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		hpp-gik
+endif
+
+ifeq (+,$(HPP_GIK_DEPEND_MK)) # --------------------------------------
+
+PREFER.hpp-gik?=	robotpkg
+
+SYSTEM_SEARCH.hpp-gik=\
+	include/hppGik/core/hppGikSolver.h	\
+	lib/libhppGik.la
+
+DEPEND_USE+=		hpp-gik
+
+DEPEND_ABI.hpp-gik?=	hpp-gik>=2.4
+DEPEND_DIR.hpp-gik?=	../../wip/hpp-gik
+
+endif # HPP_GIK_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
