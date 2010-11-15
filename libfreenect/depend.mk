25620e9feb9849aa8023d3aa43d7c871afd9333b
Matthieu Herrb <matthieu.herrb@laas.fr>
2010-11-15 14:07:31 +0100

    [libfreenect] add a depend.mk file.

---
+++ libfreenect/depend.mk
@@ -0,0 +1,26 @@
+# robotpkg depend.mk for:	hardware/libfreenect
+# Created:			Matthieu Herrb on Mon, 15 Nov 2010
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+RMP_LIBS_DEPEND_MK:=${RMP_LIBS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		libfreenect
+endif
+
+ifeq (+,$(RMP_LIBS_DEPEND_MK))
+PREFER.libfreenect?=	robotpkg
+
+DEPEND_USE+=		libfreenect
+
+DEPEND_ABI.libfreenect?=	libfreenect>=0
+DEPEND_DIR.libfreenect?=	../../hardware/libfreenect
+
+SYSTEM_SEARCH.libfreenect=\
+	bin/libfreenect-glview \
+	include/libfreenect/libfreenect.h \
+	lib/libfreenect.a
+endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
