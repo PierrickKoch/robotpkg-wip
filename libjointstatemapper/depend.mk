116018996f14f15b4f320baf8e094f208e9c59c7
Séverin Lemaignan <severin.lemaignan@laas.fr>
2011-04-13 17:39:28 +0200

    [wip/libjointstatemapper] Release of libjointstatemapper-1.0
    libJointStateMapper is a library that maps a joint configuration from one joint
    convention to another one.
    
    It relies at initialization on two lists of joint name (one for the source, one
    for the target) and a mapping between these two.
    
    Then, it can be invoked to 'reorder' a joint state (ori::JointState) from the
    source convention to the destination convention.

---
+++ libjointstatemapper/depend.mk
@@ -0,0 +1,24 @@
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+JOINTSTATEMAPPER_DEPEND_MK:=	${JOINTSTATEMAPPER_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		jointStateMapper
+endif
+
+ifeq (+,$(JOINTSTATEMAPPER_DEPEND_MK)) # ----------------------------------
+
+PREFER.jointStateMapper?=	robotpkg
+
+SYSTEM_SEARCH.jointStateMapper=\
+	include/jointStateMapper/jointStateMapper.hpp	\
+	lib/libjointstatemapper.so
+
+DEPEND_USE+=		jointStateMapper
+
+DEPEND_ABI.jointStateMapper?=jointStateMapper>=1.0
+DEPEND_DIR.jointStateMapper?=../../wip/jointStateMapper
+
+endif # JOINTSTATEMAPPER_DEPEND_MK ----------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
