00ae70e0130c8b854c55076c0a27d783a9c44b14
Nizar Sallem <nksallem@laas.fr>
2011-07-20 12:19:50 +0200

    [wip/flann] update to 1.6.11
    While there add missing depend.mk
    
    Changelog:
    Version 1.6.11 (26 June 2011)
    - bug fixes
    
    Version 1.6.10 (16 May 2011)
    - fixed a radiusSearch bug introduced in 1.6.9
    
    Version 1.6.9
    - bug fixes
    - fixed radius search bug on MSVC compiler
    - fixed windows linking problems
    
    Version 1.6.8 (26 February 2011)
    - bug fixes, low dimensional search speedup

---
+++ flann/depend.mk
@@ -0,0 +1,29 @@
+# robotpkg depend.mk for:	math/flann
+# Created:			Nizar Sallem on Wed, 20 Jul 2011
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+FLANN_DEPEND_MK:=	${FLANN_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		flann
+endif
+
+ifeq (+,$(FLANN_DEPEND_MK)) # -------------------------------------------
+
+PREFER.flann?=	robotpkg
+
+DEPEND_USE+=		flann
+DEPEND_ABI.flann?=	flann>=1.6.11
+DEPEND_DIR.flann?=	../../wip/flann
+
+SYSTEM_SEARCH.flann=\
+	include/flann/flann.h		\
+	include/flann/flann.hpp	\
+	lib/libflann.so					\
+	lib/libflann_cpp.so			\
+	lib/libflann_cpp-gd.so
+
+endif # FLANN_DEPEND_MK -------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
