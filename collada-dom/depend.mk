4ca18bf1f7758724e5a1aa6ed439420e3106ade9
Lancelot Francois <francois.lancelot@laas.fr>
2011-09-19 15:20:11 +0200

    [wip/collada-dom] Fix depend.mk

--- collada-dom/depend.mk
+++ collada-dom/depend.mk
@@ -1,25 +1,25 @@
-# robotpkg depend.mk for:	graphics/collada_dom
+# robotpkg depend.mk for:	graphics/collada-dom
 # Created:			Francois Lancelot on Thur, 01 Sep 2011
 #
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 COLLADADOM_DEPEND_MK:=		${COLLADADOM_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		collada_dom 
+DEPEND_PKG+=		collada-dom 
 endif
 
 ifeq (+,$(COLLADADOM_DEPEND_MK)) # ------------------------------------------------
 
-PREFER.collada_dom?=		robotpkg
+PREFER.collada-dom?=		robotpkg
 
-SYSTEM_SEARCH.collada_dom=\
-	include/collada_dom/dae.h	\
-	lib/collada15dom.so
+SYSTEM_SEARCH.collada-dom=\
+	include/collada-dom/dae.h	\
+	lib/libcollada15dom.so
 
-DEPEND_USE+=		collada_dom 
+DEPEND_USE+=		collada-dom 
 
-DEPEND_ABI.pqp?=	collada_dom >=2.3.1
-DEPEND_DIR.pqp?=	../../graphics/collada_dom
+DEPEND_ABI.collada-dom?=	collada-dom >=2.3.1
+DEPEND_DIR.collada-dom?=	../../wip/collada-dom
 
 endif # COLLADADOM_DEPEND_MK ------------------------------------------------------
 
