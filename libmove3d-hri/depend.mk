6f7bfeb4fdbab8953ff645064f7e02bfc4964c25
Jim Mainprice <jmainpri@aoide.laas.fr>
2011-09-29 16:39:54 +0200

    [wip/libmove3d-hri] libhri is now called libmove3d-hri, bump to version 1.99
    New naming style to keep reference to the root project
    Work on mightabilities and entity managment
    Add a new agent named Xavier (for novela festival)

---
+++ libmove3d-hri/depend.mk
@@ -0,0 +1,27 @@
+# robotpkg Makefile for:	path/libmove3d-hri
+# Created:			Jim Mainprice on Fri, 15 Apr 2011
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+LIBMOVE3D_HRI_DEPEND_MK:=	${LIBMOVE3D_HRI_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		libmove3d-hri
+endif
+
+ifeq (+,$(LIBMOVE3D_HRI_DEPEND_MK)) # ----------------------------------
+
+PREFER.libhri?=	robotpkg
+
+SYSTEM_SEARCH.libmove3d-hri=\
+	include/libmove3d/hri/hri.h	\
+	lib/libmove3d-hri.so
+
+DEPEND_USE+=		libmove3d-hri
+
+DEPEND_ABI.libmove3d-hri?=libmove3d-hri>=1.0
+DEPEND_DIR.libmove3d-hri?=../../wip/libmove3d-hri
+
+endif # LIBMOVE3D_HRI_DEPEND_MK ----------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
