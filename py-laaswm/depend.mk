853e010ce669ea80861f3244859866264bcfe3db
Séverin Lemaignan <skadge@gmail.com>
2011-10-19 21:52:20 +0200

    [wip/py-laaswm] Fixed depend.mk

--- py-laaswm/depend.mk
+++ py-laaswm/depend.mk
@@ -3,18 +3,18 @@ DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 PYLAASWM_DEPEND_MK:=	${PYLAASWM_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		pylaaswm
+DEPEND_PKG+=		py-laaswm
 endif
 
 ifeq (+,$(PYLAASWM_DEPEND_MK)) # -----------------------------------------------
 
-PREFER.pylaaswm?=		robotpkg
+PREFER.py-laaswm?=		robotpkg
 
-DEPEND_USE+=		pylaaswm
-DEPEND_ABI.pylaaswm?=	pylaaswm>=1.3
-DEPEND_DIR.pylaaswm?=	../../wip/pylaaswm
+DEPEND_USE+=		py-laaswm
+DEPEND_ABI.py-laaswm?=	${PKGTAG.python-}laaswm>=1.3
+DEPEND_DIR.py-laaswm?=	../../wip/py-laaswm
 
-SYSTEM_SEARCH.pylaaswm=\
+SYSTEM_SEARCH.py-laaswm=\
 	'${PYTHON_SYSLIBSEARCH}/laaswm.py'
 
 include ../../mk/sysdep/python.mk
