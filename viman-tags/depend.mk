bb8740411653609c8533b9babcbc9b9390453e49
Séverin Lemaignan <severin.lemaignan@laas.fr>
2012-01-25 16:01:59 +0100

    [wip/viman-tags] Updated the Viman tag set.

--- viman-tags/depend.mk
+++ viman-tags/depend.mk
@@ -1,16 +1,3 @@
-# Copyright (c) 2010 LAAS/CNRS
-# All rights reserved.
-#
-# Redistribution  and  use in source   and binary forms,  with or without
-# modification, are permitted provided that  the following conditions are
-# met:
-#
-#   1. Redistributions  of  source code must  retain  the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#   2. Redistributions in binary form must  reproduce the above copyright
-#      notice,  this list of  conditions and  the following disclaimer in
-#      the  documentation   and/or  other  materials   provided with  the
-#      distribution.
 #
 #                                    Severin Lemaignan on Tue 04 Jan 2011
 
@@ -26,7 +13,7 @@ ifeq (+,$(VIMANTAGS_DEPEND_MK)) # ----------------------------------------------
 PREFER.viman-tags?=		robotpkg
 
 DEPEND_USE+=		viman-tags
-DEPEND_ABI.viman-tags?=	viman-tags>=20110106
+DEPEND_ABI.viman-tags?=	viman-tags>=20120125
 DEPEND_DIR.viman-tags?=	../../wip/viman-tags
 
 SYSTEM_SEARCH.viman-tags=\
