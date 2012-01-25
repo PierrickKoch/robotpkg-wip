4b422ec0c2a506c5c2b3b848190245a217f1b98a
Séverin Lemaignan <severin.lemaignan@laas.fr>
2012-01-25 14:22:56 +0100

    [wip/spark-genom] New upstream version 3.5
    Changes since 3.4:
    
    - Fix drawing function (put decoration under LOGO flag)
    - Add new functions to compute distance between ROBOT and a given agent
      and to disable collision checking with HERAKLES arms.
    
    - Hypothesis On position ~ Inferrence management
    - Monitoring Spheres management
    - Change SA computation params through request
    
    - Requests for Agent State Analysis
    - Fix for detecting two humans from kinect
    - Request for show object fact
    
    While here:
    - Expose 'simulation' as option for spark
    - Removed old patches (merged upstream)

--- spark-genom/depend.mk
+++ spark-genom/depend.mk
@@ -1,18 +1,4 @@
-# Copyright (c) 2010 LAAS/CNRS
-# All rights reserved.
-#
-# Redistribution  and  use in source   and binary forms,  with or without
-# modification, are permitted provided that  the following conditions are
-# met:
-#
-#   1. Redistributions  of  source code must  retain  the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#   2. Redistributions in binary form must  reproduce the above copyright
-#      notice,  this list of  conditions and  the following disclaimer in
-#      the  documentation   and/or  other  materials   provided with  the
-#      distribution.
-#
-#                                    Severin Lemaignan on Fri 24 Sep 2010
+# Created:                         Severin Lemaignan on Fri 24 Sep 2010
 #
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
@@ -33,7 +19,7 @@ SYSTEM_SEARCH.spark-genom=\
 
 DEPEND_USE+=		spark-genom
 
-DEPEND_ABI.spark-genom?=spark-genom>=3.4
+DEPEND_ABI.spark-genom?=spark-genom>=3.5
 DEPEND_DIR.spark-genom?=../../wip/spark-genom
 
 endif # SPARK-GENOM_DEPEND_MK ----------------------------------------
