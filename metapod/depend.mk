04caa71c0a0bc65710b1ea01d91341bd3b17b1ba
Antonio El Khoury <aelkhour@laas.fr>
2012-02-29 18:20:42 +0100

    [wip/metapod] Add metapod package in initial release 1.0.0.
      MetaPOD is a template-based rigid body dynamics library.

---
+++ metapod/depend.mk
@@ -0,0 +1,27 @@
+# robotpkg depend.mk for:	math/metapod
+# Created:			Antonio El Khoury on Wed, 29 Feb 2012
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+METAPOD_DEPEND_MK:=	${METAPOD_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		metapod
+endif
+
+ifeq (+,$(METAPOD_DEPEND_MK)) # ---------------------------
+
+PREFER.metapod?=	robotpkg
+
+DEPEND_USE+=		metapod
+
+DEPEND_ABI.metapod?=	metapod>=1.0.0
+DEPEND_DIR.metapod?=	../../math/metapod
+
+SYSTEM_SEARCH.metapod=			\
+	include/metapod/algos/rnea.hh	\
+	'lib/pkgconfig/metapod.pc:/Version/s/[^0-9.]//gp'
+
+endif # METAPOD_DEPEND_MK ---------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
