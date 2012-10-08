7d581a31ff024d20dd6ea1afe641ef8cd5af1cdc
Anthony Mallet <anthony.mallet@laas.fr>
2012-10-08 19:03:44 +0200

    [wip/roscpp-core] Add a depend.mk

---
+++ roscpp-core/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	sysutils/py-rospkg
+# Created:			Anthony Mallet on Sun, 15 Jul 2012
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+ROSCPP_CORE_DEPEND_MK:=	${ROSCPP_CORE_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		roscpp-core
+endif
+
+ifeq (+,$(ROSCPP_CORE_DEPEND_MK)) # ----------------------------------------
+
+PREFER.roscpp-core?=	robotpkg
+
+DEPEND_USE+=		roscpp-core
+
+DEPEND_ABI.roscpp-core?=	roscpp-core>=0.2.6
+DEPEND_DIR.roscpp-core?=	../../wip/roscpp-core
+
+SYSTEM_SEARCH.roscpp-core=\
+	include/ros/types.h				\
+	lib/libcpp_common.so				\
+	'lib/pkgconfig/cpp_common.pc:/Version/s/[^0-9.]//gp'
+
+endif # ROSCPP_CORE_DEPEND_MK ----------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
