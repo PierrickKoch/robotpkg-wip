e882cc5d3127d03a4cc8a08786ee791871ee6a30
Anthony Mallet <anthony.mallet@laas.fr>
2012-10-29 17:55:50 +0100

    [wip/ros-genpy] Update depend.mk
    . Add a -config.cmake in SYSTEM_SEARCH
    
    . Export GEN{MSG,SRV}_PY_BIN in depend.mk

--- ros-genpy/depend.mk
+++ ros-genpy/depend.mk
@@ -20,8 +20,15 @@ DEPEND_DIR.ros-genpy?=	../../wip/ros-genpy
 
 SYSTEM_SEARCH.ros-genpy=\
 	bin/genmsg_py.py					\
+	bin/gensrv_py.py					\
+	share/genpy/cmake/genpy-config.cmake			\
 	'share/genpy/stack.xml:/<version>/s/[^0-9.]//gp'	\
 	'lib/pkgconfig/genpy.pc:/Version/s/[^0-9.]//gp'
+
+export GENMSG_PY_BIN=	${PREFIX.ros-genpy}/bin/genmsg_py.py
+export GENSRV_PY_BIN=	${PREFIX.ros-genpy}/bin/gensrv_py.py
+CMAKE_ARGS+=		-DGENMSG_PY_BIN=${GENMSG_PY_BIN}
+CMAKE_ARGS+=		-DGENSRV_PY_BIN=${GENSRV_PY_BIN}
 
 endif # ROS_GENPY_DEPEND_MK ------------------------------------------------
 
