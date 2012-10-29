dc14716b6891caee26507bf9a56039cf986c397b
Anthony Mallet <anthony.mallet@laas.fr>
2012-10-29 17:45:01 +0100

    [wip/roscpp-core] Add a bunch of %-config.cmake to SYSTEM_SEARCH

--- roscpp-core/depend.mk
+++ roscpp-core/depend.mk
@@ -19,9 +19,13 @@ DEPEND_ABI.roscpp-core?=	roscpp-core>=0.2.6
 DEPEND_DIR.roscpp-core?=	../../wip/roscpp-core
 
 SYSTEM_SEARCH.roscpp-core=\
-	include/ros/types.h				\
-	lib/libcpp_common.so				\
-	'lib/pkgconfig/cpp_common.pc:/Version/s/[^0-9.]//gp'
+  include/ros/types.h							\
+  lib/libcpp_common.so							\
+  share/cpp_common/cmake/cpp_common-config.cmake			\
+  share/roscpp_serialization/cmake/roscpp_serialization-config.cmake	\
+  share/rostime/cmake/rostime-config.cmake				\
+  share/roscpp_traits/cmake/roscpp_traits-config.cmake			\
+  'lib/pkgconfig/cpp_common.pc:/Version/s/[^0-9.]//gp'
 
 endif # ROSCPP_CORE_DEPEND_MK ----------------------------------------------
 
