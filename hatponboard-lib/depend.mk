5706be1c6d028bdca434bcc6fb109bdf4f37b351
Raphaël Lallement <raphael.lallement@laas.fr>
2013-05-24 15:10:31 +0200

    [hatponboard-lib] Set of libraries and the parser needed by HATP
    hatponboard-lib aims at splitting HATP into small parts.
    Thus reducing compilation time.
    It contains a library to represent the world and the plans while planning.
    It also contains a library to compute certain stages of planning process.
    Finally it include the parser that turns a domain file and
    the associated cost file into file the compile with HATPOnboard.

---
+++ hatponboard-lib/depend.mk
@@ -0,0 +1,31 @@
+# robotpkg depend.mk for:	planning/hatponboard-lib
+# Created:			Raphaël Lallement, 22/05/2013
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+HATPONBOARD-LIB_DEPEND_MK:=${HATPONBOARD-LIB_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		hatponboard-lib
+endif
+
+ifeq (+,$(HATPONBOARD-LIB_DEPEND_MK))
+PREFER.hatpconsole?=	robotpkg
+
+DEPEND_USE+=		hatponboard-lib
+
+DEPEND_ABI.hatponboard-lib?=	hatponboard-lib>=1.0.0
+DEPEND_DIR.hatponboard-lib?=	../../wip/hatponboard-lib
+
+SYSTEM_SEARCH.hatponboard-lib=\
+	bin/hatpparser\
+	include/hatponboard-lib/Entity.hh\
+	lib/libhatp-tools.so\
+	lib/libhatp-plan.so\
+	lib/libhatp-planningsession.so\
+	lib/pkgconfig/hatponboard-lib.pc\
+	share/cmake/Modules/Findhatponbaord-lib.pc
+endif
+
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
