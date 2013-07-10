6699ab44ec79eedf978c45ba87ff903b5abba0ee
Anthony Mallet <anthony.mallet@laas.fr>
2013-07-10 15:21:26 +0200

    [wip/jrl-dynamics-urdl] Make this compile and polish the package
    Add two local patches to make this compile. The purpose is to remove the use of
    rosbuild in the makefiles. The strategy here is to use plain cmake. However,
    catkin is also an option. In any case, this should be merged somehow upstream
    before inclusion in robotpkg.
    
    Also added a PLIST, fixed the depend.mk and added a few more dependencies that
    are now handled by robotpkg (ros-robot-model and bullet).
    
    Also note that the package compiles fine but was not tested at runtime. In
    particular, the test target is missing some data file.

--- jrl-dynamics-urdf/depend.mk
+++ jrl-dynamics-urdf/depend.mk
@@ -2,25 +2,27 @@
 # Created:			Olivier Stasse on Fri, Jun 28
 #
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-JRL_DYNAMICS_URDF_DEPEND_MK:=${JRL_DYNAMICS_URDF_DEPEND_MK}+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+JRL_DYNAMICS_URDF_DEPEND_MK:=	${JRL_DYNAMICS_URDF_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		jrl-dynamics-urdf
+DEPEND_PKG+=			jrl-dynamics-urdf
 endif
 
 ifeq (+,$(JRL_DYNAMICS_DEPEND_MK)) # ---------------------------------
 
-include ../../meta-pkgs/ros-base/depend.common
-PREFER.jrl-dynamics-urdf?=		${PREFER.ros-base}
-SYSTEM_PREFIX.jrl-dynamics-urdf?=	${SYSTEM_PREFIX.ros-base}
+PREFER.jrl-dynamics-urdf?=	robotpkg
 
-DEPEND_USE+=		jrl-dynamics-urdf
+DEPEND_USE+=			jrl-dynamics-urdf
 
-DEPEND_ABI.jrl-dynamics-urdf?=jrl-dynamics-urdf>=1.0.0
-DEPEND_DIR.jrl-dynamics-urdf?=../../wip/jrl-dynamics-urdf
+DEPEND_ABI.jrl-dynamics-urdf?=	jrl-dynamics-urdf>=1.0.0
+DEPEND_DIR.jrl-dynamics-urdf?=	../../wip/jrl-dynamics-urdf
+
+SYSTEM_SEARCH.jrl-dynamics-urdf=\
+  'bin/display-robot'			\
+  'include/jrl/dynamics/urdf/parser.hh'	\
+  'lib/libjrl-dynamics-urdf.so'
 
 endif # JRL_DYNAMICS_URDF_DEPEND_MK ---------------------------------------
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
-
+DEPEND_DEPTH:=			${DEPEND_DEPTH:+=}
