22249d20a941fde80a7fbe8990cb8e67e32890c3
Arnaud Degroote <arnaud.degroote@laas.fr>
2013-10-22 17:26:51 +0200

    [wip] Move track_target-genom to track-target-genom (debian policy)

--- track-target-genom/depend.mk
+++ track-target-genom/depend.mk
@@ -1,4 +1,4 @@
-# robotpkg depend.mk for:	action/track_target-genom
+# robotpkg depend.mk for:	action/track-target-genom
 # Created:			Arnaud Degroote on Wed, 16 Nov 2011
 #
 
@@ -6,18 +6,18 @@ DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 TRACKTARGETGENOM_DEPEND_MK:=	${TRACKTARGETGENOM_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		track_target-genom
+DEPEND_PKG+=		track-target-genom
 endif
 
 ifeq (+,$(TRACKTARGETGENOM_DEPEND_MK))
-PREFER.track_target-genom?=	robotpkg
+PREFER.track-target-genom?=	robotpkg
 
-DEPEND_USE+=		track_target-genom
+DEPEND_USE+=		track-target-genom
 
-DEPEND_ABI.track_target-genom?=	track_target-genom>=0.2
-DEPEND_DIR.track_target-genom?=	../../wip/track_target-genom
+DEPEND_ABI.track-target-genom?=	track-target-genom>=0.2
+DEPEND_DIR.track-target-genom?=	../../wip/track-target-genom
 
-SYSTEM_SEARCH.track_target-genom=\
+SYSTEM_SEARCH.track-target-genom=\
 	include/track_target/track_targetStruct.h		\
 	lib/pkgconfig/track_target.pc
 
