e553d6ffdec7a14e352eee304dcfe7e41619bdfa
Charles Lesire <charles.lesire@onera.fr>
2014-04-07 10:21:46 +0200

    Orocos DOT service package
    The rtt_dot_service is an RTT service which generates a file in
    the DOT language containing an overview of your current deployment.

---
+++ orocos-dot_service/depend.mk
@@ -0,0 +1,25 @@
+# robotpkg depend.mk for:	graphics/orocos-dot_service
+# Created:			Charles Lesire on Mon, 9 Sep 2013
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+OROCOS_DOT_DEPEND_MK:=${OROCOS_DOT_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		orocos-dot_service
+endif
+
+ifeq (+,$(OROCOS_DOT_DEPEND_MK))
+PREFER.orocos-dot_service?=	robotpkg
+
+DEPEND_USE+=		orocos-dot_service
+
+DEPEND_ABI.orocos-dot_service?=	orocos-dot_service>=1.0
+DEPEND_DIR.orocos-dot_service?=	../../onera/orocos-dot_service
+
+SYSTEM_SEARCH.orocos-dot_service= \
+	lib/orocos/gnulinux/rtt_dot_service/plugins/librtt_dot_service-gnulinux.so \
+	lib/pkgconfig/rtt_dot_service-gnulinux.pc
+endif
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
