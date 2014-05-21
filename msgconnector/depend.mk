81e204430722ed9902a5a3032b3082408696fdda
Raphaël Lallement <raphael.lallement@laas.fr>
2014-05-21 19:19:36 +0200

    [wip/msgconnector] Update to v2.8.3
    This version allows to send broadcast message and fix minor bugs

--- msgconnector/depend.mk
+++ msgconnector/depend.mk
@@ -14,7 +14,7 @@ PREFER.msgconnector?=	robotpkg
 
 DEPEND_USE+=		msgconnector
 
-DEPEND_ABI.msgconnector?=	msgconnector>=2.8.2
+DEPEND_ABI.msgconnector?=	msgconnector>=2.8.3
 DEPEND_DIR.msgconnector?=	../../wip/msgconnector
 
 SYSTEM_SEARCH.msgconnector=\
